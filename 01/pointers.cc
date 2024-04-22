259ca6cd784fe32b83bd56af4d47243a491f396b
Igor Dimitrov <igor-dimi@protonmail.com>
2024-04-22 13:34:26 +0200

    fundamentals chapter notes and code

--- 01/pointers.cc
+++ 01/pointers.cc
@@ -1,3 +1,20 @@
 #include <iostream>
 
-int i = 5;
+int main(int argc, char const *argv[])
+{
+    int i = 5;
+    int *p1 = &i;
+    int *p2 = new int;
+
+    std::cout << "i: " << i << std::endl
+              << "*p1: " << *p1 << std::endl
+              << "p1: " << p1 << std::endl
+              << "&p1: " << &p1 << std::endl
+              << "p2: " << p2 << std::endl
+              << "*p2: " << *p2 << std::endl;
+    
+    delete p2;
+    return 0;
+}
+
+
