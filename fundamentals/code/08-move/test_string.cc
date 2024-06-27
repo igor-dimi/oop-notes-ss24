59450459cc0d4a2ed1089dbb515bfe30390968dc
Igor Dimitrov <igor-dimi@protonmail.com>
2024-06-27 02:19:51 +0200

    test string move book chapter 2 notes

--- fundamentals/code/08-move/test_string.cc
+++ fundamentals/code/08-move/test_string.cc
@@ -1,5 +1,27 @@
 #include "string.h"
 #include <vector>
+#include <string>
+#include <utility>
+
+
+void foo(std::string&& rv)
+{
+    rv = "no way";
+    return ;
+}
+std::string bar(void)
+{
+    return "hello";
+}
+
+void f(int& a)
+{
+    a = 5;
+}
+
+void g(std::string str) {;}
+
+
 
 int main(int argc, char const *argv[])
 {
@@ -50,9 +72,98 @@ int main(int argc, char const *argv[])
         String s2 = std::move(s);
         std::cout << "s2: " << s2 << std::endl;
         std::cout << "s: " << s << std::endl;
-    
+    }
+
+    {
+
+        using namespace std;
+        cout << endl << "block 1" << endl;
+        String s{"hello"};
+        cout << "s: " << s << endl;
+        String&& r1{move(s)};
+        cout << "s: " << s << endl;
+        cout << "r1: " << r1 << endl;
+        String s2{move(s)};
+        cout << "s: " << s << endl;
+        cout << "s2: " << s2 << endl;
+        String&& r2{move(s2)};
+        String s3(r2);
+        cout << "s2: " << s2 << endl;
+        cout << "s3: " << s3 << endl;
+    }
+
+    {
+        using namespace std;
+        {
+           cout << endl << "block 2" << endl; 
+
+           string s1{"hello"};
+           cout << s1 << endl;
+
+           string&& r1{move(s1)};
+           string s2{r1};
+           cout << "s1: " << s1 << endl;
+           cout << "s2: " << s2 << endl;
+
+           string s3{move(s1)};
+           cout << "s1: " << s1 << endl;
+           cout << "s2: " << s2 << endl;
+
+
+        } // namespace std;
+    }
+
+    {
+        using namespace std;
+        {
+           cout << endl << "block 3" << endl; 
+
+           string s{"hello"};
+           string&& r1 = move(s);
+           string s2(move(s));
+           cout << "s2: " << s2 << endl;
+           cout << "s: " << s << endl;
+           r1 = "good";
+           cout << "s: " << s << endl;
+        } // namespace std:
+    }
+
+    {
+        using namespace std;
+        cout << endl << "block 4" << endl; 
+
+        foo("hello");
+        string s{"hello"};
+        string&& ref = move(s);
+        // foo(s);
+        foo(move(s));
+        foo(bar());
+        // foo(ref); this also doesn't work for some reason
+
+        cout << s << endl;
 
     }
+
+    {
+        using namespace std;
+        cout << endl << "block 5" << endl;
+        int a = 3;
+        f(a);
+        cout << "a: " << a << endl;
+        string s = "there";
+        g("hey");
+        g(s);
+        g(move(s));
+        cout << "s: " << s << endl;        
+        g(bar());
+        string&& ref = move(s);
+        string&& ref2 = "there";
+        string&& r3 = bar();
+
+
+
+    }
+
 
 
     return 0;
